4e33151cd6e04fad8768ba37ad093a4593a642c7
felipefo <felipefo@users.noreply.github.com>
2018-03-19 07:28:29 -0300

    .
    .

---
+++ todo_list_camadas_materializecss/www/js/controle/atividadeControle.js
@@ -0,0 +1,39 @@
+
+function AtividadeControle(){
+	
+	this.persistencia= new PersistenciaLocal();
+	this.atividadeVisao = new AtividadeVisao();
+	
+	this.atualizarLista =function(){	 
+	    var lista = this.persistencia.listarTodos();				
+		this.atividadeVisao.carregarTudo(lista);
+	}
+	
+	this.remover =function(id){		
+		this.persistencia.remover(id);
+		this.atualizarLista();
+	}
+	
+	this.salvar =function(node){
+			
+		var atividade = this.atividadeVisao.getAtividade(node);
+	    var lista = this.persistencia.salvar(atividade);
+		this.atualizarLista();
+	}
+	
+	this.adicionarAtividade = function(){        
+		var atividade = this.atividadeVisao.getNovaAtividade();		
+		atividade.status =0;
+        this.persistencia.salvar(atividade);		
+		this.atualizarLista();			    		
+    }
+    
+    this.limparTudo = function(){
+        this.persistencia.limpar();
+		this.atualizarLista();
+		
+	    
+    }    	
+}
+
+
---
+++ todo_list_camadas_sem_visual/www/js/persistencia/persistenciaLocal.js
@@ -0,0 +1,23 @@
+function PersistenciaLocal(){     
+	this.limpar = function(){	
+	   localStorage.clear();	     	   	   
+	}	
+	this.adicionar =  function(atividade){	
+	  atividade.status=0;	
+	  localStorage.setItem(atividade.id , JSON.stringify(atividade));		   
+	}	
+	this.salvar =  function(atividade){		  
+	  localStorage.setItem(atividade.id , JSON.stringify(atividade));		   
+	}
+	this.remover = function(id){	
+	   localStorage.removeItem(id);		   
+	}	
+	this.listarTodos =  function(){			
+		var lista=[];		
+		for ( var i = 0; i < localStorage.length; i++ ) {
+			var text  = localStorage.getItem(localStorage.key(i));
+			lista.push(JSON.parse(text));
+		}			
+        return lista;
+	}		    
+}

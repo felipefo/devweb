712dc91cae22b001def0226697a942d31576e404
felipefo <felipefo@users.noreply.github.com>
2019-11-13 15:57:52 -0300

    .
    .

--- Exemplo_TodoList_SLIM_V3/app/src/routes.php
+++ Exemplo_TodoList_SLIM_V3/app/src/routes.php
@@ -64,61 +64,61 @@ $app->get('/tarefas', function ($request, $response, $args) {
 		        
     });
 
-$slimSettings = array('determineRouteBeforeAppMiddleware' => true);
-$slimConfig = array('settings' => $slimSettings);
-$app = new Slim\App($slimConfig);
-
-// Check the user is logged in when necessary.
-$loggedInMiddleware = function ($request, $response, $next) {
-    $route = $request->getAttribute('route');
-    $routeName = $route->getName();
-    $groups = $route->getGroups();
-    $methods = $route->getMethods();
-    $arguments = $route->getArguments();
-
-    # Define routes that user does not have to be logged in with. All other routes, the user
-    # needs to be logged in with.
-    $publicRoutesArray = array(
-        'login',
-        'post-login',
-        'register',
-        'forgot-password',
-        'register-post',
-        'tarefas'
-    );
-
-    if (!isset($_SESSION['USER']) && !in_array($routeName, $publicRoutesArray))
-    {
-        // redirect the user to the login page and do not proceed.
-        $response = $response->withRedirect('/login');
-    }
-    else
-    {
-        // Proceed as normal...
-        $response = $next($request, $response);
-    }
-
-    return $response;
-};
-
-// Apply the middleware to every request.
-//$app->add($loggedInMiddleware);
-
-
-// Define app routes
-
-// Show the logged in dashboard page
-$app->get('/', function (Slim\Http\Request $request, \Slim\Http\Response $response, $args) {
-    // redirect the user to the logged in page.
-    $homeController = new HomeController($request, $response, $args);
-    return $homeController->index();
-})->setName('home');
-
-
-// Show the login page
-$app->get('/login', function (Slim\Http\Request $request, \Slim\Http\Response $response, $args) {
-    $homeController = new HomeController($request, $response, $args);
-    return $homeController->index();
-})->setName('login');
-
-//});
+//$slimSettings = array('determineRouteBeforeAppMiddleware' => true);
+//$slimConfig = array('settings' => $slimSettings);
+//$app = new Slim\App($slimConfig);
+//
+//// Check the user is logged in when necessary.
+//$loggedInMiddleware = function ($request, $response, $next) {
+//    $route = $request->getAttribute('route');
+//    $routeName = $route->getName();
+//    $groups = $route->getGroups();
+//    $methods = $route->getMethods();
+//    $arguments = $route->getArguments();
+//
+//    # Define routes that user does not have to be logged in with. All other routes, the user
+//    # needs to be logged in with.
+//    $publicRoutesArray = array(
+//        'login',
+//        'post-login',
+//        'register',
+//        'forgot-password',
+//        'register-post',
+//        'tarefas'
+//    );
+//
+//    if (!isset($_SESSION['USER']) && !in_array($routeName, $publicRoutesArray))
+//    {
+//        // redirect the user to the login page and do not proceed.
+//        $response = $response->withRedirect('/login');
+//    }
+//    else
+//    {
+//        // Proceed as normal...
+//        $response = $next($request, $response);
+//    }
+//
+//    return $response;
+//};
+//
+//// Apply the middleware to every request.
+////$app->add($loggedInMiddleware);
+//
+//
+//// Define app routes
+//
+//// Show the logged in dashboard page
+//$app->get('/', function (Slim\Http\Request $request, \Slim\Http\Response $response, $args) {
+//    // redirect the user to the logged in page.
+//    $homeController = new HomeController($request, $response, $args);
+//    return $homeController->index();
+//})->setName('home');
+//
+//
+//// Show the login page
+//$app->get('/login', function (Slim\Http\Request $request, \Slim\Http\Response $response, $args) {
+//    $homeController = new HomeController($request, $response, $args);
+//    return $homeController->index();
+//})->setName('login');
+//
+////});

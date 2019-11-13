522d9a6a8bc02d20ec897b0c35dacbf82bad489d
felipefo <felipefo@users.noreply.github.com>
2019-11-13 16:21:36 -0300

    .

--- Exemplo_TodoList_SLIM_V3/app/src/middleware.php
+++ Exemplo_TodoList_SLIM_V3/app/src/middleware.php
@@ -3,41 +3,14 @@
 
  //e.g: $app->add(new \Slim\Csrf\Guard);
 
-
-
 $slimSettings = array('determineRouteBeforeAppMiddleware' => true);
 $slimConfig = array('settings' => $slimSettings);
 $app = new Slim\App($slimConfig);
 
  //Check the user is logged in when necessary.
 $loggedInMiddleware = function ($request, $response, $next) {
-    
-   
-    
-    $route = $request->getAttribute('route');
-    var_dump($route->getMethods());
-    
-    //var_dump($route);    
-    $routeName = $route->getName();
-    var_dump($routeName);    
-    $groups = $route->getGroups();
-    $methods = $route->getMethods();
-    $arguments = $route->getArguments();
-
-    # Define routes that user does not have to be logged in with. All other routes, the user
-    # needs to be logged in with.
-    $publicRoutesArray = array(
-        'login',
-        'post-login',
-        'register',
-        'forgot-password',
-        'register-post',
-        'public/tarefas'
-    );
-
-   
-    if (!in_array($routeName, $publicRoutesArray))
-    //if (!isset($_SESSION['USER']) && !in_array($routeName, $publicRoutesArray))
+                       
+    if (!isset($_SESSION['USER']))
     {
         // redirect the user to the login page and do not proceed.
         //$response = $response->withRedirect('/login');
@@ -45,7 +18,7 @@ $loggedInMiddleware = function ($request, $response, $next) {
     else
     {
          // Proceed as normal...
-        $response = $next($request, $response);
+        $response = $next($request, $response); //cadeia de responsabilidade.
     }
 
     return $response;

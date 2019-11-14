4dd649a5d871ca65163a7df01ef2cd640c8e6e22
felipefo <felipefo@users.noreply.github.com>
2019-11-14 15:59:28 -0300

    Update Application.php

--- exemploMVC/exemploMVC/lib/Application.php
+++ exemploMVC/exemploMVC/lib/Application.php
@@ -1,11 +1,4 @@
 <?php
-function spl_autoload_register($st_class)
-{
-	if(file_exists('lib/'.$st_class.'.php'))
-		require_once 'lib/'.$st_class.'.php';
-}
-
-
 
 /**
 * @package Exemplo simples com MVC
